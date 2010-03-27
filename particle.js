16dc3b2d664c4e19abb0a4f93b25748d8fb8a1c5
Robert <robert@oatlab.com>
2010-03-27 00:28:59 -0400

    removing first lame example

--- particle.js
+++ particle.js
@@ -1,37 +1,3 @@
-/*
-var P = new FullBrowserCanvasAnimation({
-// some code to try out spiritfootprint
-var P = new FullBrowserCanvasAnimation({
-	onInit : function () {
-		this.particleSet = new ParticleSet(this.ctx);
-	},
-	onResize : function () {
-		this.ctx.fillStyle = 'blue';
-	},
-	onDraw : function (ctx) {
-		this.clearCtx();
-
-		// filter out mousemoves that had no button press
-		var mouseDownMouseMoves = this.getMouseDownMoves();
-		// seed mouseMoves
-		mouseDownMouseMoves.unshift(this.lastMouseMove || mouseDownMouseMoves[0]);
-
-		// add new particles
-		mouseDownMouseMoves.forEach(function (move,i,mouseMoves) {
-			if (i === 0) {
-				return;
-			}
-			this.particleSet.addParticle(new SpiritFootprint(move,mouseMoves[i - 1]));
-		},this);
-
-		this.particleSet.process();
-
-		// store the last mouse move, for next time
-		this.lastMouseMove = mouseDownMouseMoves[mouseDownMouseMoves.length - 1];
-	}
-});
-*/
-
 var P = new FullBrowserCanvasAnimation({
 	onInit : function () {
 		this.particleSet = new ParticleSet(this.ctx);
